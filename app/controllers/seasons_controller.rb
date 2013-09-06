a23a8b470c208919c2102d36964c4514ecc35a12
Ørjan Taule <o.taule@gmail.com>
2013-09-06 20:11:00 +0200

    struggling with initial data

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -3,14 +3,11 @@ class SeasonsController < ApplicationController
     @season = Season.last
     @all_seasons = Season.all
     @users = User.all.sort
+    if @season.workdays.nil?
+      @season.init_workdays_for_season
+      @season.save
+    end
     @workdays = @season.workdays
   end
 
-  def create_days_for_current_season
-    @season = Season.last
-    if @season.workdays.nil?
-      @season.init_workdays_for_season
-    end
-    redirect_to index
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,4 @@ Vaktliste::Application.routes.draw do
   patch "workdays/:id" => "workdays#update"
   post "availability_statuses/:id" => "availability_statuses#ajax_update"
 
-  get "seasons/days/create" => "seasons#create_days_for_current_season"
-
-
 end
