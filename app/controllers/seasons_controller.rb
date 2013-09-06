cf256cf08c62e7d6a1e7d0c7c82df2ce7d1f3c3b
Ørjan Taule <o.taule@gmail.com>
2013-09-06 20:37:45 +0200

    Removed unneccesary variable for seasons

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,13 +1,7 @@
 class SeasonsController < ApplicationController
   def index
     @season = Season.last
-    @all_seasons = Season.all
     @users = User.all.sort
-    if @season.workdays.to_a
-      @season.init_workdays_for_season
-      @season.save
-    end
-    @workdays = @season.workdays
   end
 
 end
